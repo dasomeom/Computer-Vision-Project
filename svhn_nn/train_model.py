f8e32438f2a335c3a2a318aa3f61b21b7a2302ed
cbanks34@gatech.edu <Chewie_Alex@nder1>
2019-10-21 18:41:22 -0400

    trained CNN on SVHN dataset

---
+++ svhn_nn/train_model.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import numpy as np
